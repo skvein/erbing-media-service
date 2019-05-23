9b1dfd97f407b63093f67e9b5712acdcd31d35f3
skevin <584473028@qq.com>
2019-05-23 17:55:07 +0800

    Initial commit

---
+++ src/main/java/com/erbing/erbingmediaservice/ErbingMediaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.erbing.erbingmediaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErbingMediaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErbingMediaServiceApplication.class, args);
+    }
+
+}
